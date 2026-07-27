22858cd0d1700d86a29e9d9b976324064145f6c6
João <joaovictorcmendonca@gmail.com>
2026-07-27 11:02:05 -0300

    .

---
+++ Python/ad-hoc/ExchangingCards.py
@@ -0,0 +1,13 @@
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    la = set(map(int, input().split()))
+    lb = set(map(int, input().split()))
+    if len(la) >= len(lb):
+        qtd = lb - la
+    else:
+        qtd = la - lb
+    print(len(qtd))
+    
+    
